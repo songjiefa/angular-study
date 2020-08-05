96618897aa57cd1132915d915196c7c36e79d0e7
songjiefa <714737954@qq.com>
2020-08-05 22:08:39 +0800

    add form submit

--- src/app/cart/cart/cart.component.ts
+++ src/app/cart/cart/cart.component.ts
@@ -1,18 +1,34 @@
 import { CartService } from './../../cart.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+    selector: 'app-cart',
+    templateUrl: './cart.component.html',
+    styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  items;
-  constructor(private carService:CartService) { }
+    items;
+    checkoutForm: FormGroup;
+    constructor(
+        private carService: CartService,
+        private formBuilder: FormBuilder,
+    ) {
+        this.checkoutForm = this.formBuilder.group({
+            name: '',
+            address: ''
+        });
+     }
 
-  ngOnInit(): void {
-      this.items = this.carService.getItems();
-  }
+    ngOnInit(): void {
+        this.items = this.carService.getItems();
+    }
+
+    onSubmit(customerData): void{
+        this.items = this.carService.clearCart();
+        this.checkoutForm.reset();
+        console.warn('Your order has been submitted', customerData);
+    }
 
 }
